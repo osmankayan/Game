2032ae0b60510187d766adb2a153facae269359b
osmankayan <96666731+osmankayan@users.noreply.github.com>
2022-04-02 17:11:29 +0300

    Add files via upload

---
+++ Minefield game/Form1.Designer.cs
@@ -0,0 +1,123 @@
+﻿namespace zNetFramework.S19.D7.DinamikFormElemanları
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnUret = new System.Windows.Forms.Button();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblSkor = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lblBulunanMayın = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnUret
+            // 
+            this.btnUret.Location = new System.Drawing.Point(544, 185);
+            this.btnUret.Name = "btnUret";
+            this.btnUret.Size = new System.Drawing.Size(105, 66);
+            this.btnUret.TabIndex = 0;
+            this.btnUret.Text = "Başla";
+            this.btnUret.UseVisualStyleBackColor = true;
+            this.btnUret.Click += new System.EventHandler(this.btnUret_Click);
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(12, 14);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(526, 423);
+            this.flowLayoutPanel1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(541, 106);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(40, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "SKOR:";
+            // 
+            // lblSkor
+            // 
+            this.lblSkor.AutoSize = true;
+            this.lblSkor.Location = new System.Drawing.Point(587, 106);
+            this.lblSkor.Name = "lblSkor";
+            this.lblSkor.Size = new System.Drawing.Size(13, 13);
+            this.lblSkor.TabIndex = 3;
+            this.lblSkor.Text = "0";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(541, 333);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(99, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "BULUNAN MAYIN:";
+            // 
+            // lblBulunanMayın
+            // 
+            this.lblBulunanMayın.AutoSize = true;
+            this.lblBulunanMayın.Location = new System.Drawing.Point(646, 333);
+            this.lblBulunanMayın.Name = "lblBulunanMayın";
+            this.lblBulunanMayın.Size = new System.Drawing.Size(13, 13);
+            this.lblBulunanMayın.TabIndex = 3;
+            this.lblBulunanMayın.Text = "0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(812, 466);
+            this.Controls.Add(this.lblBulunanMayın);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.lblSkor);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.Controls.Add(this.btnUret);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Mayın Tarlası";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnUret;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblSkor;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lblBulunanMayın;
+    }
+}
+
---
+++ Minefield game/Form1.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace zNetFramework.S19.D7.DinamikFormElemanları
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnUret_Click(object sender, EventArgs e)
+        {
+            int mayin1 = 0;
+            int mayin2 = 0;
+            int mayin3 = 0;
+            int ButonSayisi = 0;
+            Random rnd1 = new Random();
+            mayin1 = rnd1.Next(1, 20);
+            mayin2 = rnd1.Next(20, 35);
+            mayin3 = rnd1.Next(35, 50);
+            foreach (Button item in flowLayoutPanel1.Controls)
+            {
+                ButonSayisi++;
+            }
+            for (int i = 1; i < 51; i++)
+            {
+                Button btn = new Button();
+                btn.Name = "btn"+ i;
+                btn.Size= new System.Drawing.Size(55, 55);
+                btn.TabIndex = i - 1;
+                btn.Text = "Mayın" + i;
+
+               
+                if (mayin1 == i || mayin2 == i || mayin3 == i)
+                {
+                    btn.Tag = true;
+                }
+                else
+                {
+                    btn.Tag = false;
+                }
+                
+
+                if (ButonSayisi<50)
+                {
+                    flowLayoutPanel1.Controls.Add(btn);
+                }
+                btn.Click += btn_Click;
+
+            }
+            
+            
+
+        }
+        private void btn_Click(object sender, EventArgs e)
+        {
+            Button basilanbuton = (Button)sender;
+            bool mayinbulundu =(bool) basilanbuton.Tag;
+            if (mayinbulundu)
+            {
+                MessageBox.Show("Tebrikler,Mayını buldunuz!");
+                basilanbuton.BackColor = Color.Red;
+                int BulunanMayin = int.Parse(lblBulunanMayın.Text);
+                BulunanMayin++;
+                lblBulunanMayın.Text = BulunanMayin.ToString();
+
+            }
+            else
+            {
+                basilanbuton.BackColor = Color.Green;
+                int Skor = int.Parse(lblSkor.Text);
+                Skor++;
+                lblSkor.Text = Skor.ToString();
+            }
+            if (int.Parse(lblBulunanMayın.Text) == 3)
+            {
+                //flowLayoutPanel1.Enabled = false; //foreach döngüsüne girmeden de bu şekilde halledilebilir.
+                foreach (Button item in flowLayoutPanel1.Controls)
+                {
+                    item.Enabled = false;
+                }
+                MessageBox.Show("OYUN BİTTİ");
+
+                
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+
+        //this.button1.Location = new System.Drawing.Point(3, 3);
+        //      this.button1.Name = "button1";
+        //      this.button1.Size = new System.Drawing.Size(75, 23);
+        //      this.button1.TabIndex = 0;
+        //      this.button1.Text = "button1";
+        //      this.button1.UseVisualStyleBackColor = true;
+        //      this.button1.Click += new System.EventHandler(this.button1_Click);
+        //YUKARIDAKİ KOD SATIRLARI İNİTİALİZE COMPONENTS İÇİNDEN ALINAN HERHANGİ BİR BUTONUN KOD SATIRALRIDIR,YUKARIDA KULLANMAK İÇİN BURAYA YORUM SATIRI OLARAK YAZDIM
+    }
+}
